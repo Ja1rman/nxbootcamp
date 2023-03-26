3083eb4b3beff49e0f2d20d8d39829da18566fb2
Roman <49319500+Ja1rman@users.noreply.github.com>
2023-03-26 19:49:50 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         CDRparser data = new CDRparser("./data/cdr.txt");
@@ -17,7 +18,5 @@ public class Main {
             ReportCreater temp = new ReportCreater(key, arr);
             temp.outputSave();
         }
-
-
     }
 }
